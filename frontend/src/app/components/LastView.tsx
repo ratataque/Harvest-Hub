930673b32376ad5b0bc9c607bba09ef546afcfea
ratataque <ewanrimmely@outlook.fr>
2024-11-13 22:38:54 +0100

    fix title

--- frontend/src/app/components/LastView.tsx
+++ frontend/src/app/components/LastView.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 export default function LastView() {
   return (
     <div className="w-full h-[95%] flex items-center px-8 relative">
@@ -10,6 +12,15 @@ export default function LastView() {
         <source src="/videos/HarvestHubv1.3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      <Image
+        src="/images/fougere.png"
+        alt="Picture of the author"
+        width={2000}
+        height={2000}
+        // className="w-96 transform rotate-[-68deg]"
+        className="w-96 absolute scale-x-[-1] bottom-[-150px] transform rotate-[68deg] right-[-100px] z-[-1]"
+      />
     </div>
   );
 }
